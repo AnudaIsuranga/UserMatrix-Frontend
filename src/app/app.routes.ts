2336188b1b48015997e1365feb698a9e2449985b
AnudaIsuranga <isuranuda555@gmail.com>
2025-07-04 15:51:33 +0530

    chore: Configure routes for FAQs and About Us in AppRoutingModule

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,8 @@ import { Routes } from '@angular/router';
 import { AddUserComponent } from './pages/add-user/add-user.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { ManageUserComponent } from './pages/manage-user/manage-user.component';
+import { FaqsComponent } from './faqs/faqs.component';
+import { AboutUsComponent } from './about-us/about-us.component';
 
 export const routes: Routes = [
     {
@@ -15,5 +17,13 @@ export const routes: Routes = [
     {
         path:"manage-user",
         component:ManageUserComponent
-    }
+    },
+    {
+        path:"faqs",
+        component:FaqsComponent
+    },
+    {
+        path:"about-us",
+        component:AboutUsComponent
+    },
 ];
